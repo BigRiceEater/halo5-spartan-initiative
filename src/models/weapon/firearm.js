75276c4e4341e8bf73a000823525b94c99b0ba84
big.rice.eater <34292181+BigRiceEater@users.noreply.github.com>
2019-07-26 17:42:47 +0800

    Add sub-properties to firearm for organisation

--- src/models/weapon/firearm.js
+++ src/models/weapon/firearm.js
@@ -2,37 +2,49 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const firearmSchema = new Schema({
-  rateOfFire: {
-    type: Number,
-    min: 0
+  shooting: {
+    rateOfFire: {
+      type: Number,
+      min: 0
+    },
+    ammoPerShot: {
+      type: Number,
+      min: 1,
+      default: 1
+    },
+    canPickupAmmo: {
+      type: Boolean,
+      required: true
+    },
+    maxAmmo: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+    ammoPerClip: {
+      type: Number,
+      required: true,
+      min: 0
+    }
   },
-  ammoPerShot: {
-    type: Number,
-    min: 1,
-    default: 1
-  },
-  requireCharging: {
-    type: Boolean,
-    default: false
-  },
-  timeToChargeShot: {
-    type: Number,
-    min: 0,
-    default: 0
-  },
-  canPickupAmmo: {
-    type: Boolean,
-    required: true
-  },
-  maxAmmo: {
-    type: Number,
-    required: true,
-    min: 0
-  },
-  ammoPerClip: {
-    type: Number,
-    required: true,
-    min: 0
+  charging: {
+    canCharge: {
+      type: Boolean,
+      default: false
+    },
+    mustChargeToUse: {
+      type: Boolean,
+      default: false
+    },
+    timeToChargeShot: {
+      type: Number,
+      min: 0,
+      default: 0
+    },
+    immediateShotWhenCharged: {
+      type: Boolean,
+      default: true
+    }
   },
   zoom: {
     canZoom: {
